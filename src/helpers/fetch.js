f4e594ff5c92b6d5a5475c70734a03794238628a
Zchrs <gustavo.a.c.m@gmail.com>
2025-12-23 18:41:28 -0500

    fetch

--- src/helpers/fetch.js
+++ src/helpers/fetch.js
@@ -1,8 +1,7 @@
 /* eslint-disable no-debugger */
 const baseUrl = import.meta.env.VITE_APP_API_URL;
-const baseUrlAdmin = import.meta.env.VITE_APP_API_URL_ADMIN;
 
-
+// fetch usuario
  export const fetchWithoutToken = ( endpoint, data, method = "GET" ) => {
     const url = `${ baseUrl }/${ endpoint }`
     if (method === "GET") {
@@ -21,7 +20,7 @@ const baseUrlAdmin = import.meta.env.VITE_APP_API_URL_ADMIN;
 
  export const fetchWithToken = ( endpoint, data, method = "GET" ) => {
     const url = `${ baseUrl }/${ endpoint }`
-    const token = localStorage.getItem('token') || '';
+    const token = localStorage.getItem('tokenUser') || '';
 
     if (method === "GET") {
         return fetch( url,  {
@@ -44,8 +43,9 @@ const baseUrlAdmin = import.meta.env.VITE_APP_API_URL_ADMIN;
     }
 }
 
+// fetch admin
 export const fetchWithoutTokenAdmin = ( endpoint, data, method = "GET" ) => {
-    const url = `${ baseUrlAdmin }/${ endpoint }`
+    const url = `${ baseUrl }/${ endpoint }`
 
     if (method === "GET") {
         return fetch( url )
@@ -62,8 +62,8 @@ export const fetchWithoutTokenAdmin = ( endpoint, data, method = "GET" ) => {
 }
 
 export const fetchWithTokenAdmin = ( endpoint, data, method = "GET" ) => {
-    const url = `${ baseUrlAdmin }/${ endpoint }`
-    const token = localStorage.getItem('token') || '';
+    const url = `${ baseUrl }/${ endpoint }`
+    const token = localStorage.getItem('tokenAdmin') || '';
     if (method === "GET") {
         return fetch( url,  {
             method,
@@ -85,3 +85,44 @@ export const fetchWithTokenAdmin = ( endpoint, data, method = "GET" ) => {
     }
 }
 
+// fetch asesor
+export const fetchWithoutTokenAdvisor = ( endpoint, data, method = "GET" ) => {
+    const url = `${ baseUrl }/${ endpoint }`
+
+    if (method === "GET") {
+        return fetch( url )
+    } else {
+        return fetch( url, {
+            method,
+            headers: {
+                // ...corsHeaders,  
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+    }
+}
+
+export const fetchWithTokenAdvisor = ( endpoint, data, method = "GET" ) => {
+    const url = `${ baseUrl }/${ endpoint }`
+    const token = localStorage.getItem('tokenAdvisor') || '';
+    if (method === "GET") {
+        return fetch( url,  {
+            method,
+            headers: {
+                // ...corsHeaders, 
+                'x-token': token
+            }
+        })
+    } else {
+        return fetch( url, {
+            method,
+            headers: {
+                // ...corsHeaders, 
+                'content-type': 'application/json',
+                'x-token': token
+            },
+            body: JSON.stringify(data)
+        })
+    }
+}
